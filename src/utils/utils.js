7c4b6e566a9879c324597b32c6a8db1eca0a8daa
Steliann <steliann@tete-chercheuse.fr>
2020-07-10 18:34:16 +0200

    Base code

---
+++ src/utils/utils.js
@@ -0,0 +1,32 @@
+/**
+ * Extends multiple object into one
+ * @param {Boolean} deep Enable extend for deep object properties
+ * @param {Array} objects List of objects to merged
+ * @return {Object} Objects merged into one
+ */
+export const extend = (deep = false, ...objects) => {
+
+  let extended = {};
+
+  // Merge the object into the extended object
+  let merge = obj => {
+    for(let prop in obj) {
+      if(Object.prototype.hasOwnProperty.call(obj, prop)) {
+        // If deep merge and property is an object, merge properties
+        if(deep && Object.prototype.toString.call(obj[prop]) === '[object Object]') {
+          extended[prop] = this.extend(true, extended[prop], obj[prop]);
+        }
+        else {
+          extended[prop] = obj[prop];
+        }
+      }
+    }
+  }
+
+  // Loop through each object and conduct a merge
+  objects.forEach(object => {
+    merge(object);
+  });
+
+  return extended;
+};
