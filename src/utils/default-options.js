3dd4fb8541bee659d8e12e28f6d32ffca63a5f0a
Steliann <steliann@tete-chercheuse.fr>
2020-09-29 19:56:25 +0200

    Change default tiles

--- src/utils/default-options.js
+++ src/utils/default-options.js
@@ -14,10 +14,10 @@ export default {
       center: [0, 0],
     },
     tiles: {
-      url: 'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png',
+      url: 'https://{s}.basemaps.cartocdn.com/rastertiles/voyager_labels_under/{z}/{x}/{y}{r}.png',
       options: {
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attributions">CARTO</a>',
-        subdomains: 'abcd',
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+        subdomains: 'abcd'
       }
     },
     markers: {
